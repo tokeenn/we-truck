99367259995ec868849c2c0ddc9dbd849ed22bef
[pauljulienfrancobaptista] <[pauljulien.francobaptista@gmail.com]>
2023-05-04 14:45:20 +0200

    ajout de bundle webpack, joue de plusieurs fonctions pour rendre le flux rss fonctionnel

--- myproject/src/Controller/ActivitesController.php
+++ myproject/src/Controller/ActivitesController.php
@@ -2,17 +2,51 @@
 
 namespace App\Controller;
 
+use DOMDocument;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ActivitesController extends AbstractController
 {
+
+    private $listeNews;
+
+    public function __construct()
+    {
+        $this->listeNews = $this->loadFeed();        
+    }
+
+    public function getListeNews(){
+        return $this->listeNews;
+    }
+
+    private function loadFeed(){
+        $rss = new DOMDocument();
+        $rss->load("https://www.lepilote.com/fr/piloterss/Disruptions");
+        
+        
+        $feed = array();
+        foreach($rss->getElementsByTagName("item") as $key => $ligne){
+            $item = [
+                'id' => $key,
+                'category' => $ligne->getElementsByTagName("category")[0]->nodeValue,
+                'title' => $ligne->getElementsByTagName("title")[0]->nodeValue,
+                'date' => $ligne->getElementsByTagName("pubDate")[0]->nodeValue,
+                'desc' => $ligne->getElementsByTagName("description")[0]->nodeValue,
+                'link' => $ligne->getElementsByTagName("link")[0]->nodeValue,
+            ];
+            $feed[] = $item;
+        }
+        return $feed;
+    }
+
     #[Route('/activites', name: 'app_activites')]
     public function index(): Response
     {
         return $this->render('activites/index.html.twig', [
             'controller_name' => 'ActivitesController',
+            'loadFeed' => $this->loadFeed(),
         ]);
     }
 }
