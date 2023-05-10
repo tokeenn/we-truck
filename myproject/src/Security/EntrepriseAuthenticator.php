8a7ed523ecf92291e4741e32f4cd9169f52b3fd5
Stelian-Constantin Stanciu <stanciu.stelian86@yahoo.fr>
2023-05-10 11:35:31 +0200

    rajout d'un if pour la verification de role au niveau de login

--- myproject/src/Security/EntrepriseAuthenticator.php
+++ myproject/src/Security/EntrepriseAuthenticator.php
@@ -2,37 +2,64 @@
 
 namespace App\Security;
 
-use Symfony\Component\HttpFoundation\RedirectResponse;
+use App\Entity\User;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
-use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
-use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Doctrine\ORM\EntityManagerInterface;
 
-class EntrepriseAuthenticator extends AbstractLoginFormAuthenticator
+class AuthAuthenticator extends AbstractLoginFormAuthenticator
 {
+    private EntityManagerInterface $entityManager;
+    public function __construct(private UrlGeneratorInterface $urlGenerator, EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+   
     use TargetPathTrait;
 
     public const LOGIN_ROUTE = 'app_login';
 
-    public function __construct(private UrlGeneratorInterface $urlGenerator)
-    {
-    }
+
 
     public function authenticate(Request $request): Passport
     {
-        $email = $request->request->get('email', '[ROLE_ENTREPRISE]');
-
+        
+        $email = $request->request->get('email', '');
+        
         $request->getSession()->set(Security::LAST_USERNAME, $email);
+        $userLoader = function () use ($email) {
+            $userRepository = $this->entityManager->getRepository(User::class);
+            $user = $userRepository->findOneByEmail($email);
+
+            return $user;
+        };
+        $user = $userLoader();
+        if (!$user) {
+            throw new CustomUserMessageAuthenticationException('Invalid credentials.');
+        }
+    
+        // Check if the user has the ROLE_USER role
+        $userRoles = $user->getRoles();
+        if (!in_array('[ROLE_ENTREPRISE]', $userRoles)) {
+            throw new CustomUserMessageAuthenticationException('Access denied.');
+        }
+
 
         return new Passport(
-            new UserBadge($email),
+            
+            new UserBadge($email,$userLoader),
             new PasswordCredentials($request->request->get('password', '')),
             [
                 new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
@@ -47,7 +74,7 @@ class EntrepriseAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('app_accueil'));
+        return new RedirectResponse($this->urlGenerator->generate('app_dashboard'));
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
