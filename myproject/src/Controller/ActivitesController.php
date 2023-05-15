1fbb56300acf873a2c9a13e5752d1e959e922c9f
[pauljulienfrancobaptista] <[pauljulien.francobaptista@gmail.com]>
2023-05-15 10:11:29 +0200

    ajout de actualite controller ainsi que l'index avec tout le code pour le flux

--- myproject/src/Controller/ActivitesController.php
+++ myproject/src/Controller/ActivitesController.php
@@ -11,53 +11,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ActivitesController extends AbstractController
 {
 
-    private $listeNews;
-
-    public function __construct()
-    {
-        $this->listeNews = $this->loadFeed();
-    }
-
-    public function getListeNews()
-    {
-        return $this->listeNews;
-    }
-
-    private function loadFeed()
-    {
-        $rss = new DOMDocument();
-        $rss->load("https://www.lepilote.com/fr/piloterss/Disruptions");
-
-
-        $feed = array();
-        foreach ($rss->getElementsByTagName("item") as $key => $ligne) {
-            $item = [
-                'id' => $key,
-                'categ' => $ligne->getElementsByTagName("category")[0]->nodeValue,
-                'title' => $ligne->getElementsByTagName("title")[0]->nodeValue,
-                'date' => $ligne->getElementsByTagName("pubDate")[0]->nodeValue,
-                'desc' => $ligne->getElementsByTagName("description")[0]->nodeValue,
-                'link' => $ligne->getElementsByTagName("link")[0]->nodeValue,
-            ];
-            // Ajouter l'élément au tableau associatif de la catégorie correspondante
-            $category = $item['categ'];
-            if (!isset($feed[$category])) {
-                $feed[$category] = array();
-            }
-            $feed[$category][] = $item;
-        }
-        return $feed;
-    }
-
     #[Route('/activites', name: 'app_activites')]
-    public function index(Request $request ): Response
+    public function index(): Response
     {
-        $category = $request->query->get('category');
 
         return $this->render('activites/index.html.twig', [
             'controller_name' => 'ActivitesController',
-            'loadFeed' => $this->loadFeed(),
-            'category' => $category,
         ]);
     }
 }
